65472ec60f86312e71a2212d762c31543f378687
stikrobinson <148508110+stikrobinson@users.noreply.github.com>
2025-07-27 19:06:20 +0000

    guia 20

--- backend_data_server/settings.py
+++ backend_data_server/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "homepage",
+    "rest_framework",
+    "demo_api_rest",
 ]
 
 MIDDLEWARE = [
--- backend_data_server/urls.py
+++ backend_data_server/urls.py
@@ -17,10 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from homepage import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("homepage/", include("homepage.urls")),
-    path('', views.index, name='index'),
+    path("demo/api/rest/", include("demo_api_rest.urls")),
+    path("", include("homepage.urls"))
 ]
